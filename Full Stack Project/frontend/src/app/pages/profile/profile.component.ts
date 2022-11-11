f49c5a5539a541140f6b75c18185bde120a3f915
shitishpatel <68268691+shitishpatel@users.noreply.github.com>
2022-11-11 13:41:08 +0530

    Create profile.component.ts

---
+++ Full Stack Project/frontend/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router'
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  data: any;
+  constructor(private auth:AuthService,private router:Router) { }
+
+  ngOnInit(): void {
+    this.getProfile();
+  }
+
+  getProfile(){
+    this.auth.getProfile().subscribe(
+      res=>{
+        if(res.success){
+          this.data=res.message[0];
+        }else{
+          this.logout();
+        }
+      },err=>{
+        // console.log(err)
+      }
+    )
+  }
+
+
+  logout(){
+    localStorage.clear();
+    this.router.navigate(['/login'])
+  }
+
+}
